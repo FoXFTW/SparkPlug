389ad6010cc4df3794a7271aa6c8b5106ec5762f
FoXFTW <hannes@octofox.de>
2017-11-18 21:09:48 +0100

    :bug: Fix tests/Routing/RouteStringConverterTest.php

--- tests/Routing/RouteStringConverterTest.php
+++ tests/Routing/RouteStringConverterTest.php
@@ -23,7 +23,7 @@ class RouteStringConverterTest extends TestCase
     private $router;
 
     /**
-     * @covers RouteStringConverter
+     * @covers \App\SparkPlug\Routing\RouteStringConverter
      */
     public function testRegexConversion()
     {
@@ -41,7 +41,7 @@ class RouteStringConverterTest extends TestCase
 
     protected function setUp()
     {
-        $app = require __DIR__.'/../config/app.php';
+        $app = require __DIR__.'/../../config/app.php';
         $this->router = app()->make(Router::class, true);
         $this->router->get('/index', 'IndexController@test');
         $this->router->get('/user/profile', 'UserController@test');
