4876e674e45cf49651ea5a1444e846c3226e1924
FoXFTW <hannes@octofox.de>
2017-12-17 20:27:21 +0100

    :hammer: Update Controllers with Queries

--- app/Controllers/User/AccountController.php
+++ app/Controllers/User/AccountController.php
@@ -7,6 +7,7 @@
 
 namespace App\Controllers\User;
 
+use App\SparkPlug\Auth\Auth;
 use App\SparkPlug\Controllers\AbstractController as Controller;
 use App\SparkPlug\Views\View;
 
@@ -25,6 +26,48 @@ class AccountController extends Controller
      */
     public function showProfileView()
     {
-        return new View('user.profile');
+        if (!login_check()) {
+            return redirect('/login');
+        }
+
+        $view = new View('user.profile');
+        $view->setVars(
+            [
+                'rias' => app()->make(Auth::class)->getUser()->rias(),
+            ]
+        );
+
+        return $view;
     }
+
+
+    public function changePassword()
+    {
+
+        $validator = new Validation();
+
+        $data = $validator->validate(
+            [
+                'password' => 'confirmed|min:3|max:255',
+            ],
+            $this->request
+        );
+
+        $userId = app()->make(Auth::class)->getUser().user_id;
+
+        /* TODO get user, change password
+        $user->password = bcrypt($data['password']);
+
+        try {
+            $user->save();
+        } catch (\PDOException $e) {
+
+            return back();
+        }
+        }
+     */
+
+        return redirect('/profile');
+    }
+
 }
